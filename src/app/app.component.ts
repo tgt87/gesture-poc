abe0220c65080afcd88b9c4ffa138c4df9908725
Gek Teng TAN <gekteng.tan@gigrt.com>
2017-11-09 11:34:29 +0800

    added swipe to change img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +6,9 @@ import { Component, Renderer2, OnInit, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
- // title = 'app works!';
+
  eventText: string;
+ displayText: string;
  initialTouchPos: any;
  lastTouchPos: any;
 //  rafPending: boolean = false;
@@ -17,12 +18,17 @@ export class AppComponent implements OnInit {
  distThreshold: number = 100;
  diffInX: number = 0;
  diffInY: number = 0;
-  constructor(private renderer: Renderer2, private element: ElementRef){
+ imgSrc = ["cat", "owl", "test"];
+ imgIndex = 0;
+ imgToShow: string;
+ imgNo: string = "Image " + (this.imgIndex + 1).toString();
+  constructor(){
 
   }
   ngOnInit(){
     // let test = this.element.nativeElement.querySelector("#test");
     // this.renderer.listen(test, 'pointerdown', this.handleGestureStart)
+    this.imgToShow = `../assets/${this.imgSrc[0]}.jpg`;
   }
 
   handleGestureStart(event){
@@ -33,10 +39,10 @@ export class AppComponent implements OnInit {
     if(event.touches && event.touches.length > 1) return;
      
     this.initialTouchPos = this.getGesturePoint(event);
-    console.log(event.target);
+   // console.log(event.target);
     this.startTime = new Date().getTime();
     event.target.setPointerCapture(event.pointerId);
-    this.eventText = "pointer down " + this.initialTouchPos.x.toString() + "," + this.initialTouchPos.y.toString();
+   // this.eventText = "pointer down " + this.initialTouchPos.x.toString() + "," + this.initialTouchPos.y.toString();
     console.log(this.initialTouchPos);
     
   }
@@ -47,7 +53,7 @@ export class AppComponent implements OnInit {
     event.preventDefault();
     if(!this.initialTouchPos) return;
     this.lastTouchPos = this.getGesturePoint(event);
-    console.log(this.lastTouchPos);
+   // console.log(this.lastTouchPos);
     this.endTime = new Date().getTime();
     this.diffInX = this.lastTouchPos.x - this.initialTouchPos.x;
     this.diffInY = this.lastTouchPos.y - this.initialTouchPos.y;
@@ -60,18 +66,38 @@ export class AppComponent implements OnInit {
         this.eventText = 'swipe down';
       } else if(this.diffInY < 0 && Math.abs(this.diffInY) >= this.distThreshold){
         this.eventText = 'swipe up';
-      }
+      } else {
+        this.eventText = 'distance too short'
+      }  
+    } else {
+      this.eventText = 'Too slow';
     }
-    // if(this.rafPending) return;
-    // this.rafPending = true;
-    
-    
-    // window.requestAnimationFrame
+
   }
 
   handleGestureEnd(event){
     console.log('end');
-    
+    this.displayText = this.eventText;
+    event.target.releasePointerCapture(event.pointerId);
+  }
+
+  handleSwipeEnd(event){
+    if(this.eventText === 'swipe right'){
+      console.log('right');
+      if(this.imgIndex - 1 >= 0){
+        this.imgIndex -= 1;
+        this.imgToShow = `../assets/${this.imgSrc[this.imgIndex]}.jpg`;
+        this.imgNo = "Image " + (this.imgIndex + 1).toString();
+      }
+         
+    } else if (this.eventText === 'swipe left'){
+      console.log('left');
+      if(this.imgIndex + 1 <= this.imgSrc.length - 1){
+        this.imgIndex += 1;
+        this.imgToShow = `../assets/${this.imgSrc[this.imgIndex]}.jpg`;
+        this.imgNo = "Image " + (this.imgIndex + 1).toString();
+      } 
+    }
     event.target.releasePointerCapture(event.pointerId);
   }
 
